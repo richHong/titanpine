4b5d129c3656095ceda5a7ff412506345410870d
Andy Nixon <andielane@yahoo.com>
2016-05-13 19:28:44 -0700

    Adding NavBar for the Navigation Bar

---
+++ public/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class NavBar extends React.Component {
+	render(){
+		return (
+			<div>
+				<a href>Sign Up</a>
+				<a href>Log In</a>
+    			<input type="text" placeholder="Search"/>
+    			<button type="submit">Search</button>
+    		</div>
+    	)
+	}
+}
+
+export default NavBar
