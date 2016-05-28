f43daef01c8310822e091d7040a1ef18d3d01d85
Richard Hong <rich.hong@gmail.com>
2016-05-27 17:26:29 -0700

    Added carousel to single house listing and added styles.

--- public/singlelisting.js
+++ public/singlelisting.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import GMaps from './map';
+import MyCarousel from './carousel'
+
 class SingleListing extends React.Component {
 	render(){
+    console.log(this.props.listing);
 		return <div>
-					{this.props.listing.house_name}<br />
-					{this.props.listing.heading}<br />
-					{this.props.listing.street_add}<br />
-					{this.props.listing.city}, {this.props.listing.state}
-				</div>
+        <MyCarousel className='carousel'/>
+        <div className="houseInfo">
+  				<span className='houseName'><b>{this.props.listing.house_name}</b></span><br/>
+          <i>{this.props.listing.heading}</i><br/><br/>
+
+          <b>Location:</b><br/>
+          {this.props.listing.street_add}<br/>
+          {this.props.listing.city},{this.props.listing.state} {this.props.listing.zipcode}<br/><br/>
+
+          <b>Price:</b> ${this.props.listing.price} per night<br /><br/>
+
+          <b>Vacancies:</b> {this.props.listing.vacancies} <br /><br />
+
+          <b>House Interests:</b> {this.props.listing.house_interests.split(',').map((interest, i) => <span key={i} >{interest}, </span>)}<br /><br />
+
+          <b>House Mission:</b> {this.props.listing.house_mission} <br /><br />
+
+          <b>House Rules: </b> {this.props.listing.house_rules} <br /><br />
+
+        </div>
+        <GMaps />
+			</div>
 	}
 }
 
