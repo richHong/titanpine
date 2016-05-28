f8007727f5155166a2303aa7c6af078b84c16ac2
selfresonator <selfresonator@gmail.com>
2016-05-28 12:09:12 -0700

    Added webpack.production.config.js for production version

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
 'use strict';
 var express = require('express');
 var path = require('path');
+// var tls = require('tls');
 var https = require('https');
 var httpProxy = require('http-proxy');
 var path = require('path');
 var publicPath = path.resolve(__dirname, 'public');
 var bodyParser = require('body-parser');
+// var isProduction = false;
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
 var port = 3000;
 var config = require('./config.json');
 var fs = require('fs');
 var S3FS = require('s3fs');
 var multiparty = require('connect-multiparty')();
-var isProduction = false;
-
 // We need to add a configuration to our proxy server,
 // as we are now proxying outside localhost
 var proxy = httpProxy.createProxyServer({
-  changeOrigin: true
+    changeOrigin: true
 });
 
 var app = express();
 // var httpsServer = https.createServer(options, app);
-
-// var options = {
-//    key: fs.readFileSync('./key.pem', 'utf8'),
-//    cert: fs.readFileSync('./server.crt', 'utf8'),
-//    NPNProtocols: ['http/2.0', 'spdy', 'http/1.1', 'http/1.0']
-// };
+// var tlsServer = tls.createServer(options, app);
+var options = {
+   key: fs.readFileSync('./key.pem', 'utf8'),
+   cert: fs.readFileSync('./server.crt', 'utf8'),
+   requestCert: true,
+   NPNProtocols: ['http/2.0', 'spdy', 'http/1.1', 'http/1.0']
+};
 
 //serving our index.html
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extened:true}));
+app.use(bodyParser.urlencoded({extened: true}));
 
 
 //server/compiler.js runs webpack-dev-server which creates the bundle.js which index.html serves
@@ -47,58 +50,67 @@ app.use(bodyParser.urlencoded({extened:true}));
 //   });
 // });
 if (!isProduction) {
-  var bundle = require('./server/compiler.js');
-  bundle();
-  app.all('/build/*', function (req, res) {
-    proxy.web(req, res, {
-        target: 'http://localhost:8080'
+    var bundle = require('./server/compiler.js');
+    bundle();
+    app.all('/build/*', function(req, res) {
+        proxy.web(req, res, {
+            target: 'http://localhost:8080'
+        });
     });
-  });
 }
 
 proxy.on('error', function(e) {
-  e.preventDefault();
-  console.log('Could not connect to proxy, please try again...');
+    e.preventDefault();
+    console.log('Could not connect to proxy, please try again...');
 });
 
-app.listen(port, function () {
-  console.log('Server running on port ' + port);
+app.listen(port, function() {
+    console.log('Server running on port ' + port);
 });
 // httpsServer.listen(port, function () {
 //   console.log('Server running on port ' + port);
 // });
+// tls.createServer(options, function (cleartextStream) {
+//     var cleartextRequest = net.connect({
+//         port: 3000,
+//         host: '127.0.0.1'
+//     }, function () {
+//         cleartextStream.pipe(cleartextRequest);
+//         cleartextRequest.pipe(cleartextStream);
+//     });
+// }).listen(3000);
 
 
 //THIS IS ALL FILE UPLOAD STUFFFFFF=============================================
 var s3fsImplementation1 = new S3FS('hackerhabitatavatars', {
-  accessKeyId: config.accessKeyId,
-  secretAccessKey: config.secretAccessKey,
-  endpoint: config.endpoint,
-  region: config.region
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.secretAccessKey,
+    endpoint: config.endpoint,
+    region: config.region
 });
 
 var s3fsImplementation2 = new S3FS('hackerhabitatlistings', {
-  accessKeyId: config.accessKeyId,
-  secretAccessKey: config.secretAccessKey,
-  endpoint: config.endpoint,
-  region: config.region
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.secretAccessKey,
+    endpoint: config.endpoint,
+    region: config.region
 });
 
 app.use(multiparty);
-app.post('/v1/ap', function (req,res) {
-  var file = req.files.file;
-  var stream = fs.createReadStream(file.path);
+app.post('/v1/ap', function(req, res) {
+    var file = req.files.file;
+    var stream = fs.createReadStream(file.path);
 
-  return s3fsImplementation1.writeFile(file.originalFilename, stream)
-    .then(function() {
-      fs.unlink(file.path, function(err) {
-        if (err) {
-          console.log('Error');
-        }
-        console.log('Success');
-    });
-    res.send('File Upload Complete');
-  });
+    return s3fsImplementation1.writeFile(file.originalFilename, stream)
+        .then(function() {
+            fs.unlink(file.path, function(err) {
+                if (err) {
+                    console.log('Error');
+                }
+                console.log('Success');
+            });
+            res.send('File Upload Complete');
+        });
 });
 
 app.post('/v1/lp', function(req, res) {
