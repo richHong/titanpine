7839b7d4a78875ab4c7cd6c245dcd9233f3c4b3a
selfresonator <selfresonator@gmail.com>
2016-05-29 14:50:41 -0700

    Created middleware files for S3 database API

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ var publicPath = path.resolve(__dirname, 'public');
 var bodyParser = require('body-parser');
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
-var config = require('./config.json');
-var fs = require('fs');
-var S3FS = require('s3fs');
 var multiparty = require('connect-multiparty')();
 
 // We need to add a configuration to our proxy server,
@@ -22,8 +19,11 @@ var app = express();
 //serving our index.html
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extened: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+//THIS IS ALL FILE UPLOAD STUFFFFFF=============================================
+app.use(multiparty);
 
+//THIS IS ALL FILE UPLOAD STUFFFFFF=============================================
 
 //server/compiler.js runs webpack-dev-server which creates the bundle.js which index.html serves
 //the compiler adds some console logs for some extra sugar
@@ -39,57 +39,6 @@ if (!isProduction) {
         });
     });
 }
-
-
-//THIS IS ALL FILE UPLOAD STUFFFFFF=============================================
-var s3fsImplementation1 = new S3FS('hackerhabitatavatars', {
-    accessKeyId: config.accessKeyId,
-    secretAccessKey: config.secretAccessKey,
-    endpoint: config.endpoint,
-    region: config.region
-});
-
-var s3fsImplementation2 = new S3FS('hackerhabitatlistings', {
-    accessKeyId: config.accessKeyId,
-    secretAccessKey: config.secretAccessKey,
-    endpoint: config.endpoint,
-    region: config.region
-});
-
-app.use(multiparty);
-app.post('/v1/ap', function(req, res) {
-    var file = req.files.file;
-    var stream = fs.createReadStream(file.path);
-
-    return s3fsImplementation1.writeFile(file.originalFilename, stream)
-        .then(function() {
-            fs.unlink(file.path, function(err) {
-                if (err) {
-                    console.log('Error');
-                }
-                console.log('Success');
-            });
-            res.send('File Upload Complete');
-        });
-});
-
-app.post('/v1/lp', function(req, res) {
-    var file = req.files.file;
-    var stream = fs.createReadStream(file.path);
-
-    return s3fsImplementation2.writeFile(file.originalFilename, stream)
-        .then(function() {
-            fs.unlink(file.path, function(err) {
-                if (err) {
-                    console.log('Error');
-                }
-                console.log('Success');
-            });
-            res.send('File Upload Complete');
-        });
-});
-//THIS IS ALL FILE UPLOAD STUFFFFFF=============================================
-
 
 proxy.on('error', function(e) {
     console.log('Could not connect to proxy, please try again...');
