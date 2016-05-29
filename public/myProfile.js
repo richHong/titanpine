c15a7f3638abf3b2d28494db20893ac738f6bd4a
Richard Hong <rich.hong@gmail.com>
2016-05-28 17:21:00 -0700

    User profile now shows all houses made by user.

--- public/myProfile.js
+++ public/myProfile.js
@@ -6,54 +6,16 @@ import SingleProfile from './singleProfile';
 export default class MyProfile extends Component {
   constructor(){
     super();
-    this.state = {
-      favorites:[{
-        url: 'https://cdn-images-1.medium.com/max/400/1*wV0lBcrBtw3iqsCtkgZTBg.png',
-        house_name: "Telegraph Hacker House",
-        heading: "You like that, don't you!",
-        street_add: "1600 Shattuck Ave",
-        city: "Berkeley",
-        state:"CA",
-        zipcode: "94709",
-        price: "55.00"
-      },{
-        url:'http://www.socketsite.com/wp-content/uploads/2014/02/2123-2127-Castro.gif',
-        house_name: "House of Pain",
-        heading: "Insane in the brain!",
-        street_add: "123 Broadway",
-        city: "Oakland",
-        state:"CA",
-        zipcode: "94612",
-        price: "45.00"
-      },{
-        url:'http://www.insidesfre.com/wp-content/uploads/2013/04/243romain.jpg',
-        house_name: "Tech Tower",
-        heading: "It's Amazing!",
-        street_add: "23122 Fremont",
-        city: "San Jose",
-        state:"CA",
-        zipcode: "94483",
-        price: "50.00"
-      },{
-        url: 'http://noehill.com/sf/landmarks/hayes/russell_warrren_house_thumb.jpg',
-        house_name: "Digital Domain",
-        heading: "Technology is good!",
-        street_add: "4234 Broadway",
-        city: "San Francisco",
-        state:"CA",
-        zipcode: "92343",
-        price: "60.00"
-      },{
-        url:'https://cdn3.vox-cdn.com/uploads/chorus_image/image/48181361/sgijsdlfjlsd.0.0.jpg',
-        house_name: "Coder Condos",
-        heading: "Brain me!",
-        street_add: "83234 Brains",
-        city: "Mountainview",
-        state:"CA",
-        zipcode: "94234",
-        price: "40.00"
-      }]
-    };
+    this.state = {};
+  }
+  componentWillMount(){
+    let userID = window.localStorage.getItem('userID');
+
+    fetch('http://localhost:3001/v1/listings/?user_id='+userID)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({favorites: json.data});
+    });
   }
   // componentDidMount(){
   //   const TAG = 'hacker'
@@ -69,7 +31,7 @@ export default class MyProfile extends Component {
   render() {
     return (
       <div>
-        <HouseListOfListings houses={this.state.favorites} favorites='favorites'/>
+        <HouseListOfListings houses={this.state.favorites} page='profile'/>
         <SingleProfile />
         <div id='instafeed' className='instafeed'></div>
       </div>
