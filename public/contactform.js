869101e1dcab3b403a9ad5c5cf13276f9e57e488
Andy Nixon <andielane@yahoo.com>
2016-05-30 17:24:43 -0700

    Commit before pulling changes

--- public/contactform.js
+++ public/contactform.js
@@ -5,7 +5,7 @@ class ContactForm extends React.Component {
 			<form>
 				<label>Your Message:</label><br />
 				<textarea ref={(message) => this.message = message} onChange={this.onMessage.bind(this)} rows='40' cols='150'></textarea><br/>
-				<input type='submit' to="/createProfile" value='Send'/>
+				<input name='message' type='submit' to="/createProfile" value='Send'/>
 			</form>
 		)
 	}
