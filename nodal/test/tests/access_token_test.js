b8e3460ee5907b6235cd960a7a49ceeb7ed35d77
selfresonator <selfresonator@gmail.com>
2016-06-22 01:16:55 -0700

    Deleted router test and replaced it with accesstokentest

--- nodal/test/tests/access_token_test.js
+++ nodal/test/tests/access_token_test.js
@@ -3,12 +3,15 @@ module.exports = (() => {
   'use strict';
 
   const Nodal = require('nodal');
+  const Router = Nodal.require('app/router.js');
+
+  console.log(Router);
 
   class RouterTest extends Nodal.mocha.Test {
 
     test(expect) {
 
-      it('Should do something', done => {
+      it('Should return a status of', done => {
 
         expect(null).to.not.exist;
         done();
